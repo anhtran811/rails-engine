d3fd966fa816051fa333ab2b5d8231d4799618c5
Anhnhi Tran <anhtran811@gmail.com>
2023-02-06 22:52:50 -0700

    Completed endpoint for item by id

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
+
+  def show
+    if Item.exists?(params[:id])
+      render json: Item.find(params[:id])
+    else
+      render json: { errors: 'item does not exist' }, status: 404
+    end
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -50,4 +50,44 @@ describe 'Items API' do
       end
     end
   end
+
+  describe 'GET /items/:id' do
+    context 'when the item exists' do
+      it 'can get one item by its id' do
+        item = create(:item)
+
+        get "/api/v1/items/#{item.id}"
+
+        item_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(item_data).to have_key(:id)
+        expect(item_data[:id]).to be_a(Integer)
+
+        expect(item_data).to have_key(:name)
+        expect(item_data[:name]).to be_a(String)
+
+        expect(item_data).to have_key(:description)
+        expect(item_data[:description]).to be_a(String)
+
+        expect(item_data).to have_key(:unit_price)
+        expect(item_data[:unit_price]).to be_a(Float)
+
+        expect(item_data).to have_key(:merchant_id)
+        expect(item_data[:merchant_id]).to be_a(Integer)
+      end
+    end
+
+    context 'when the item does not exist' do
+      it 'responds with an error' do
+        get "/api/v1/items/1"
+
+        item = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(404)
+        expect(item).to have_key(:errors)
+        expect(item[:errors]).to match(/item does not exist/)
+      end
+    end
+  end
 end
