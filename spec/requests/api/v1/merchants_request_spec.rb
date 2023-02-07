97c64ee9bd7b911c51a8960f6585b7e5dcea7a6a
Anhnhi Tran <anhtran811@gmail.com>
2023-02-06 21:07:06 -0700

    Completed endpoint for merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,37 +1,68 @@
 require 'rails_helper' 
 
 describe 'Merchants API' do
-  context 'when merchants exist' do
-    it 'sends a list of merchants' do
-      create_list(:merchant, 5)
+  describe 'GET /merchants' do
+    context 'when merchants exist' do
+      it 'sends a list of merchants' do
+        create_list(:merchant, 5)
 
-      get '/api/v1/merchants'
-      
-      expect(response).to be_successful
-      
-      merchants = JSON.parse(response.body, symbolize_names: true)
-      
-      expect(merchants.count).to eq(5)
-      
-      merchants.each do |merchant|
-        expect(merchant).to have_key(:id)
-        expect(merchant[:id]).to be_an(Integer)
+        get '/api/v1/merchants'
         
-        expect(merchant).to have_key(:name)
-        expect(merchant[:name]).to be_an(String)
+        expect(response).to be_successful
+        
+        merchants = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(merchants.count).to eq(5)
+        
+        merchants.each do |merchant|
+          expect(merchant).to have_key(:id)
+          expect(merchant[:id]).to be_an(Integer)
+          
+          expect(merchant).to have_key(:name)
+          expect(merchant[:name]).to be_an(String)
+        end
       end
     end
-    
+      
     context 'when there are no merchants in the database' do
       it 'returns an empty array' do
         get '/api/v1/merchants'
 
         expect(response).to be_successful
 
-        merchants = JSON.parse(response.body, symbolize_name: true)
+        merchants = JSON.parse(response.body, symbolize_names: true)
 
         expect(merchants).to be_an(Array)
         expect(merchants.count).to eq(0)        
+      end
+    end
+  end
+    
+  describe 'GET merchants/:id' do
+    context 'when the merchant exists' do
+      it 'can get one merchant by their id' do
+        id = create(:merchant).id
+
+        get "/api/v1/merchants/#{id}"
+
+        merchant = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(response).to be_successful
+
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(Integer)
+        
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+      end
+    end
+    
+    context 'when the merchant does not exist' do
+      it 'responds with an error' do
+        get "/api/v1/merchants/1"
+        
+        merchant = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(404)
       end
     end
   end
