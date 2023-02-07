e98dbb1b803ad15976ae58394e7db9632c15a8dc
Anhnhi Tran <anhtran811@gmail.com>
2023-02-06 22:29:43 -0700

    Adds Merchant Items Controller

---
+++ app/controllers/api/v1/merchant/items_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::Merchant::ItemsController < ApplicationController
+  def index
+    if Merchant.exists?(params[:merchant_id])
+      render json: Item.all
+    else
+      render json: { errors: /merchant item does not exist/}, status: 404
+    end
+  end
+end
