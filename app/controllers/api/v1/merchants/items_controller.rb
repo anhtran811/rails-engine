d11b71bae6d160d7feee840043d4811c54353acf
Anhnhi Tran <anhtran811@gmail.com>
2023-02-07 23:23:17 -0700

    Refactor merchant items index to use rescue block

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    if Merchant.exists?(params[:merchant_id])
+    
+    # if Merchant.exists?(params[:merchant_id])
       render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
-    else
-      render json: { errors: /merchant item does not exist/}, status: 404
-    end
+    # else
+    #   render json: { errors: /merchant item does not exist/}, status: 404
+    # end
   end
 end
