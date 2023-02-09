fc5506db39ae22eafe9ea366c91f2366b0236963
Anhnhi Tran <anhtran811@gmail.com>
2023-02-08 23:20:49 -0700

    Completed search all merchants by name model test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,9 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices, dependent: :destroy
 
   validates :name, presence: true 
+
+  def self.search_all_by_name(name_search)
+    where("name ILIKE ?", "%#{name_search}%")
+    .order(:name)
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'class methods' do
+    describe ":search_all_by_name" do
+      it 'returns all merchants, alphabeticaly, with case-insensitive and partial search parameters' do
+        merchant_1 = create(:merchant, name: "Harry Potter")
+        merchant_2 = create(:merchant, name: "Hermione Granger")
+        merchant_3 = create(:merchant, name: "Ronald Weasley")
+        merchant_4 = create(:merchant, name: "Severus Snape")
+
+        expect(Merchant.search_all_by_name("er")).to eq([merchant_1, merchant_2, merchant_4])
+      end
+    end
+  end
 end
