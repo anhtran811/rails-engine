a6dce07abde24b55aa7bbf6002935a4bb0e72acf
Anhnhi Tran <anhtran811@gmail.com>
2023-02-09 18:07:03 -0700

    Deleted commented out lines

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,11 +4,7 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    # if Item.exists?(params[:id])
-      render json: ItemSerializer.new(Item.find(params[:id]))
-    # else
-    #   # render json: { errors: 'item does not exist' }, status: 404
-    # end
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def create
